b369979a84b72fb383927df8c83504e6c5a05459
Sam Ulyanoff <uyanov@list.ru>
2026-06-14 14:22:44 +0300

    Initial commit

---
+++ src/test/java/ru/microservices/myApp/MyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.microservices.myApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
