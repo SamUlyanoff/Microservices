db39c04cfed49af9a07f5cef7b017127c40a344c
Sam Ulyanoff <uyanov@list.ru>
2026-07-27 07:27:36 +0300

    Сделал ченджсеты для создания таблиц под Entity user and UserPassword

--- auth-service/src/main/java/ru/microservices/auth_service/entity/UserPassword.java
+++ auth-service/src/main/java/ru/microservices/auth_service/entity/UserPassword.java
@@ -2,11 +2,15 @@ package ru.microservices.auth_service.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import java.time.Instant;
 import java.util.UUID;
 
 @Entity
@@ -16,9 +20,16 @@ import java.util.UUID;
 @NoArgsConstructor
 public class UserPassword {
 
+    @Id
     @Column(name = "user_id")
     private UUID userId;
     @Column(name = "user_password")
     private String hashedPassword;
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private Instant createdAt;
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Instant updatedAt;
 
 }
--- user-service/src/main/java/ru/microservices/user_service/entity/User.java
+++ user-service/src/main/java/ru/microservices/user_service/entity/User.java
@@ -8,6 +8,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.Instant;
 import java.util.UUID;
@@ -31,5 +32,8 @@ public class User {
     @CreationTimestamp
     @Column(name = "created_at")
     private Instant createdAt;
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Instant updatedAt;
 
 }
