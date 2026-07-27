b1c6d3307824a836115f61cf53c7eda86128fc53
Sam Ulyanoff <uyanov@list.ru>
2026-07-27 04:09:58 +0300

    Сделал отдельную событийную библиотеку

---
+++ common-events/src/main/java/ru/microservices/common_events/UserCreatedEvent.java
@@ -0,0 +1,9 @@
+package ru.microservices.common_events;
+
+import lombok.Builder;
+
+import java.time.Instant;
+
+@Builder
+public record UserCreatedEvent(String id, String firstName, String lastName, String email, Character[] password, Instant createdAt) {
+}
