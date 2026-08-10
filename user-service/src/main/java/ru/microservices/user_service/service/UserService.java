6dac7736cd0214d0108c74839122208b69a6e4df
Sam Ulyanoff <uyanov@list.ru>
2026-08-10 18:09:14 +0300

    Десериализация внутри топика

--- user-service/src/main/java/ru/microservices/user_service/service/UserService.java
+++ user-service/src/main/java/ru/microservices/user_service/service/UserService.java
@@ -1,12 +1,15 @@
 package ru.microservices.user_service.service;
 
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.microservices.common_events.UserCreatedEvent;
 import ru.microservices.user_service.mapper.UserMapper;
 import ru.microservices.user_service.repository.UserRepository;
+import tools.jackson.databind.ObjectMapper;
 
 @Service
 @AllArgsConstructor
@@ -14,11 +17,15 @@ public class UserService {
 
     private final UserMapper userMapper;
     private final UserRepository userRepository;
+    private final ObjectMapper objectMapper;
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @KafkaListener(topics = "user.created")
     @Transactional
-    public void createUser(UserCreatedEvent userCreatedEvent){
+    public void createUser(String payload){
+        UserCreatedEvent userCreatedEvent = objectMapper.readValue(payload, UserCreatedEvent.class);
         userRepository.save(userMapper.mapUser(userCreatedEvent));
+        logger.info("Пользователь с email {} создан", userCreatedEvent.email());
     }
 
     /**
