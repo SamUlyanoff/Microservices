2482fe1517d794126fe1dd58a70cff81e6cc33fa
Sam Ulyanoff <uyanov@list.ru>
2026-08-10 19:20:58 +0300

    Валидация

--- auth-service/src/main/java/ru/microservices/auth_service/models/UserRequest.java
+++ auth-service/src/main/java/ru/microservices/auth_service/models/UserRequest.java
@@ -1,8 +1,33 @@
 package ru.microservices.auth_service.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 import java.util.Arrays;
 
-public record UserRequest(String firstName, String lastName, String email, Character[] password) {
+public record UserRequest(
+        @Pattern(
+                regexp = "^[А-Яа-яA-Za-z]+$",
+                message = "Введённое имя не соответствует требованиям"
+        )
+        @Size(min = 1, max = 50, message = "Размер вводимого имени не соответствует границам [0; 50] символов")
+        String firstName,
+
+        @Pattern(
+                regexp = "^[А-Яа-яA-Za-z]+$",
+                message = "Введённая фамилия не соответствует требованиям"
+        )
+        @Size(min = 1, max = 50, message = "Размер вводимой фамилии не соответствует границам [0; 50] символов")
+        String lastName,
+
+        @NotBlank(message = "Email не может быть пустым")
+        @Email(message = "Введённый email не соответствует требованиям")
+        @Size(max = 75, message = "Размер email не должен превышать 75 символов")
+        String email,
+
+        Character[] password) {
 
     @Override
     public String toString() {
