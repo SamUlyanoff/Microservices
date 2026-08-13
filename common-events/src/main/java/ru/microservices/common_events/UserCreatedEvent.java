2064352fc8f48c1f13aaa2bde3ccfb6d711e261d
Sam Ulyanoff <uyanov@list.ru>
2026-08-13 04:55:28 +0300

    Орфография и toString для UserCreatedEvent

--- common-events/src/main/java/ru/microservices/common_events/UserCreatedEvent.java
+++ common-events/src/main/java/ru/microservices/common_events/UserCreatedEvent.java
@@ -6,4 +6,15 @@ import java.time.Instant;
 
 @Builder
 public record UserCreatedEvent(String id, String firstName, String lastName, String email, Instant createdAt) {
+
+    @Override
+    public String toString() {
+        return "UserCreatedEvent{" +
+                "\nid = '" + id + '\'' +
+                ",\nfirstName = '" + firstName + '\'' +
+                ",\nlastName = '" + lastName + '\'' +
+                ",\nemail = '" + email + '\'' +
+                ",\ncreatedAt = " + createdAt +
+                '}';
+    }
 }
